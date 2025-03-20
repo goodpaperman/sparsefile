1405cb2fd027aa3c1c50d8e3db6f476e867e9dcd
yunhai.win <haihai107@126.com>
2025-03-20 10:23:15 +0800

    make SetFileInformationByHandle separate with SetFileValidData or sparse-file

--- sparsefile.cpp
+++ sparsefile.cpp
@@ -6,16 +6,17 @@
 #include "c_timer.h"
 #include "priv.h"
 
-int USE_SET_END_OF_FILE = 0; 
+int USE_FILE_END_OF_FILE_INFO = 0; 
 int USE_SET_FILE_VALID_DATA = 0;
-int WRITE_WHOLE_FILE = 0;
+int USE_SPARSE_FILE = 1; 
+int WRITE_FILE_MODE = 0;
 char FILL_CHAR = '\0'; 
 
 int main(int argc, char* argv[])
 {
     if (argc < 3)
     {
-        std::cout << "Usage: sparsefile file length(in GB) [set-end-of-file] [set-file-valid-data] [write-whole-file] [fill-char]\n";
+        std::cout << "Usage: sparsefile file length(in GB) [set-file-end-of-file-info] [set-file-valid-data] [sparse-file] [write-file-mode 0|1|2] [fill-char]\n";
         return 1; 
     }
 
@@ -25,16 +26,19 @@ int main(int argc, char* argv[])
     long long file_size = atoll(argv[2]) * 1024 * 1024 * 1024;  // unit in GB
 
     if (argc > 3)
-        USE_SET_END_OF_FILE = atoi(argv[3]);
+        USE_FILE_END_OF_FILE_INFO = atoi(argv[3]);
 
     if (argc > 4)
         USE_SET_FILE_VALID_DATA = atoi(argv[4]);
 
     if (argc > 5)
-        WRITE_WHOLE_FILE = atoi(argv[5]);
+        USE_SPARSE_FILE = atoi(argv[5]);
 
     if (argc > 6)
-        FILL_CHAR = argv[6][0];
+        WRITE_FILE_MODE = atoi(argv[6]);
+
+    if (argc > 7)
+        FILL_CHAR = argv[7][0];
 
     do
     {
@@ -57,114 +61,107 @@ int main(int argc, char* argv[])
             break;
         }
 
-        if (USE_SET_END_OF_FILE)
-        {
-            pos.QuadPart = file_size;
-            if (::SetFilePointerEx(file_handle, pos, NULL, FILE_BEGIN) == 0)
-            {
-                std::cout << "SetFilePointerEx failed, error " << GetLastError() << std::endl;
-                ret = 5;
-                break;
-            }
-
-            if (!::SetEndOfFile(file_handle))
-            {
-                std::cout << "SetEndOfFile failed, error " << GetLastError() << std::endl;
-                ret = 6;
-                break;
-            }
-
-            if (USE_SET_FILE_VALID_DATA)
-            {
-                if (!::SetFileValidData(file_handle, pos.QuadPart - 1))
-                {
-                    std::cout << "SetFileValidData failed, error " << GetLastError() << std::endl;
-                    ret = 11;
-                    break;
-                }
-            }
-            else
-            {
-                DWORD temp = 0;
-                if (!::DeviceIoControl(file_handle, FSCTL_SET_SPARSE, NULL, 0, NULL, 0, &temp, NULL))
-                {
-                    std::cout << "DeviceIoControl failed, error " << GetLastError() << std::endl;
-                    ret = 12;
-                    break;
-                }
-            }
-
-            if (WRITE_WHOLE_FILE)
-            {
-                // write whole file
-                long long file_size = pos.QuadPart;
-                pos.QuadPart = 0;
-                if (::SetFilePointerEx(file_handle, pos, NULL, FILE_BEGIN) == 0)
-                {
-                    std::cout << "SetFilePointerEx failed, error " << GetLastError() << std::endl;
-                    ret = 20;
-                    break;
-                }
-
-                char buf[4096] = { FILL_CHAR };
-                c_timer t;
-                DWORD bytes = 0;
-                for (long long i = 0; i < file_size; i += 4096)
-                {
-                    if (!::WriteFile(file_handle, buf, 4096, &bytes, NULL) || bytes != 4096)
-                    {
-                        std::cout << "WriteFile failed, error " << GetLastError() << ", written " << bytes << std::endl;
-                        ret = 21;
-                        break;
-                    }
-                }
-
-                int elapse = t.get_interval();
-                std::cout << "write file elapse " << elapse << " ms" << std::endl;
-            }
-            else
-            {
-                // write a byte at end
-                pos.QuadPart--;
-                if (::SetFilePointerEx(file_handle, pos, NULL, FILE_BEGIN) == 0)
-                {
-                    std::cout << "SetFilePointerEx failed, error " << GetLastError() << std::endl;
-                    ret = 30;
-                    break;
-                }
-
-                DWORD bytes = 0;
-                c_timer t;
-                char buf[1] = { FILL_CHAR };
-                if (!::WriteFile(file_handle, buf, 1, &bytes, NULL) || bytes != 1)
-                {
-                    std::cout << "WriteFile failed, error " << GetLastError() << ", written " << bytes << std::endl;
-                    ret = 31;
-                    break;
-                }
-
-                int elapse = t.get_interval();
-                std::cout << "write file elapse " << elapse << " ms" << std::endl;
-            }
-        }
-        else
+        if (USE_FILE_END_OF_FILE_INFO)
         {
             FILE_END_OF_FILE_INFO info;
             info.EndOfFile.QuadPart = file_size;
             if (!SetFileInformationByHandle(file_handle, FileEndOfFileInfo, &info, sizeof(info)))
             {
                 std::cout << "SetFileInformationByHandle failed, error " << GetLastError() << std::endl;
+                ret = 5;
+                break;
+            }
+        }
+        else
+        {
+            pos.QuadPart = file_size;
+            if (::SetFilePointerEx(file_handle, pos, NULL, FILE_BEGIN) == 0)
+            {
+                std::cout << "SetFilePointerEx failed, error " << GetLastError() << std::endl;
                 ret = 3;
                 break;
             }
 
+            if (!::SetEndOfFile(file_handle))
+            {
+                std::cout << "SetEndOfFile failed, error " << GetLastError() << std::endl;
+                ret = 4;
+                break;
+            }
+        }
+
+        if (USE_SET_FILE_VALID_DATA)
+        {
+            if (!::SetFileValidData(file_handle, file_size - 1))
+            {
+                std::cout << "SetFileValidData failed, error " << GetLastError() << std::endl;
+                ret = 11;
+                break;
+            }
+        }
+
+        if (USE_SPARSE_FILE)
+        {
             DWORD temp = 0;
             if (!::DeviceIoControl(file_handle, FSCTL_SET_SPARSE, NULL, 0, NULL, 0, &temp, NULL))
             {
                 std::cout << "DeviceIoControl failed, error " << GetLastError() << std::endl;
-                ret = 4;
+                ret = 20;
                 break;
             }
+
+            std::cout << "set sparse file ok" << std::endl;
+        }
+
+        DWORD bytes = 0;
+        if (WRITE_FILE_MODE == 1)
+        {
+            // write a byte at end
+            pos.QuadPart = file_size - 1;
+            if (::SetFilePointerEx(file_handle, pos, NULL, FILE_BEGIN) == 0)
+            {
+                std::cout << "SetFilePointerEx failed, error " << GetLastError() << std::endl;
+                ret = 30;
+                break;
+            }
+
+            c_timer t;
+            char buf[1] = { FILL_CHAR };
+            if (!::WriteFile(file_handle, buf, 1, &bytes, NULL) || bytes != 1)
+            {
+                std::cout << "WriteFile failed, error " << GetLastError() << ", written " << bytes << std::endl;
+                ret = 31;
+                break;
+            }
+
+            int elapse = t.get_interval();
+            std::cout << "write file end a byte elapse " << elapse << " ms" << std::endl;
+        }
+        else if (WRITE_FILE_MODE == 2)
+        {
+            // write whole file
+            pos.QuadPart = 0;
+            if (::SetFilePointerEx(file_handle, pos, NULL, FILE_BEGIN) == 0)
+            {
+                std::cout << "SetFilePointerEx failed, error " << GetLastError() << std::endl;
+                ret = 32;
+                break;
+            }
+
+            c_timer t;
+            char buf[4096] = { FILL_CHAR };
+            for (long long i = 0; i < file_size; i += 4096)
+            {
+                if (!::WriteFile(file_handle, buf, 4096, &bytes, NULL) || bytes != 4096)
+                {
+                    std::cout << "WriteFile failed, error " << GetLastError() << ", written " << bytes << std::endl;
+                    ret = 21;
+                    break;
+                }
+            }
+
+            int elapse = t.get_interval();
+            std::cout << "write whole file elapse " << elapse << " ms" << std::endl;
         }
     } while (0); 
 
